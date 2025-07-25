8d08b99a744a1a8e4e8a6dd8e8b68f002c97b01d
MuammerKRKS <muhde99.mk@gmail.com>
2025-07-25 14:20:17 +0300

    Add files via upload

---
+++ salaryCalculation/src/main/java/salaryCalculation/salaryCalculation.java
@@ -0,0 +1,8 @@
+package salaryCalculation;
+
+public class salaryCalculation {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Mehmet Sait", 2000.0, 45, 2005);
+        System.out.println(emp.toString());
+    }
+}
